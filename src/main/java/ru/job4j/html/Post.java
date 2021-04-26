ed000323d0e30d709fba4890ab8b787fbb9d4f4d
astolbin <astolbin@ya.ru>
2021-04-26 08:07:14 +0300

    2.2. Модель данных - Post. [#345962]

---
+++ src/main/java/ru/job4j/html/Post.java
@@ -0,0 +1,49 @@
+package ru.job4j.html;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private String name;
+    private String text;
+    private String link;
+    private LocalDateTime create;
+
+    public Post(String name, String text, String link, LocalDateTime create) {
+        this.name = name;
+        this.text = text;
+        this.link = link;
+        this.create = create;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public LocalDateTime getCreate() {
+        return create;
+    }
+
+    public void setCreate(LocalDateTime create) {
+        this.create = create;
+    }
+}
